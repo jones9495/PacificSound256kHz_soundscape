4ab1286f1fddf33ef24f5bc889cafbbc6b659cba
Jones Cleetus <jonescleetus@Joness-iMac.local>
2025-09-16 11:20:06 +0530

    PSD for May 2021

--- data/scheduler.py
+++
@@ -1,8 +0,0 @@
-# scheduler.py
-# Small helper that can be used to produce scheduled jobs later.
-# For now it's a convenience wrapper around slot_generator.
-from slot_generator import generate_slots
-
-def get_slots_for_doctor(doctor_phone, days=3):
-    # in a real app you might vary by doctor schedule; here we return a generic list
-    return generate_slots(days=days, slots_per_day=4)
